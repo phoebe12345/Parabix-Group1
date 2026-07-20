024b76069967a850d88b3f70139be65aae2808f2
phoebe12345 <phoebehan888@gmail.com>
2026-07-20 12:57:42 -0700

    Add SVE2 expand

--- include/idisa/idisa_arm_sve2_builder.h
+++ include/idisa/idisa_arm_sve2_builder.h
@@ -3,26 +3,15 @@
 #include <idisa/idisa_arm_builder.h>
 
 namespace IDISA {
-
-// SVE2-capable ARM builder.
-//
-// This is a part of Phase 3: for now it inherits every NEON implementation
-// from IDISA_ARM_Builder, so functionally it behaves exactly like
-// the plain NEON builder. right now is just to exist as a
-//  separately selectable class 
-
 class IDISA_ARM_SVE2_Builder : public IDISA_ARM_Builder {
 public:
     IDISA_ARM_SVE2_Builder(llvm::LLVMContext & C, const FeatureSet & featureSet, unsigned bitBlockWidth, unsigned laneWidth);
 
     std::string getBuilderUniqueName() override;
-
-    // SVE2-specific compress
-    
     llvm::Value * mvmd_compress(unsigned fw, llvm::Value * a, llvm::Value * select_mask) override;
+    llvm::Value * mvmd_expand(unsigned fw, llvm::Value * a, llvm::Value * select_mask) override;
 
     ~IDISA_ARM_SVE2_Builder() override {}
 };
 
 }
-
--- lib/idisa/idisa_arm_sve2_builder.cpp
+++ lib/idisa/idisa_arm_sve2_builder.cpp
@@ -55,6 +55,7 @@ Value * IDISA_ARM_SVE2_Builder::mvmd_compress(unsigned fw, Value * a, Value * se
         Value * scalableResult = CreateCall(compact->getFunctionType(), compact,
                                              {scalablePred, scalableData});
 
+        // Extract the low 16 lanes back into our fixed-width representation.
         Function * extractResult = Intrinsic::getDeclaration(getModule(), Intrinsic::vector_extract,
                                                                {fixed16xi8Ty, scalable16xi8Ty});
         Value * fixedResult = CreateCall(extractResult->getFunctionType(), extractResult,
@@ -62,6 +63,58 @@ Value * IDISA_ARM_SVE2_Builder::mvmd_compress(unsigned fw, Value * a, Value * se
         return fwCast(8, fixedResult);
     }
     return IDISA_ARM_Builder::mvmd_compress(fw, a, select_mask);
+}
+
+Value * IDISA_ARM_SVE2_Builder::mvmd_expand(unsigned fw, Value * a, Value * select_mask) {
+    if (mBitBlockWidth == 128 && fw == 8) {
+        const unsigned fieldCount = 16;
+        Type * i8Ty = getInt8Ty();
+        auto * fixed16xi8Ty = FixedVectorType::get(i8Ty, fieldCount);
+        auto * scalable16xi8Ty = ScalableVectorType::get(i8Ty, 16);
+
+        Value * maskBits = CreateZExtOrTrunc(select_mask, getInt16Ty());
+        Value * selectedBytes = UndefValue::get(fixed16xi8Ty);
+        for (unsigned i = 0; i < fieldCount; i++) {
+            Value * bit = CreateAnd(CreateLShr(maskBits, ConstantInt::get(getInt16Ty(), i)),
+                                     ConstantInt::get(getInt16Ty(), 1));
+            Value * isSelBit = CreateICmpNE(bit, ConstantInt::get(getInt16Ty(), 0));
+            Value * asByte = CreateSExt(isSelBit, i8Ty); // 0xFF or 0x00
+            selectedBytes = CreateInsertElement(selectedBytes, asByte, ConstantInt::get(getInt32Ty(), i));
+        }
+        Value * isSelected = CreateICmpNE(selectedBytes, ConstantAggregateZero::get(fixed16xi8Ty));
+
+        Value * ones = CreateLShr(selectedBytes, getSplat(fieldCount, getInt8(7)));
+        Value * inclusiveRank = hsimd_partial_sum(8, ones);
+        Value * rank = simd_sub(8, inclusiveRank, ones);
+
+        Value * gatherIdx = CreateSelect(isSelected, rank, ConstantAggregateZero::get(fixed16xi8Ty));
+
+        Function * insertData = Intrinsic::getDeclaration(getModule(), Intrinsic::vector_insert,
+                                                            {scalable16xi8Ty, fixed16xi8Ty});
+        Value * fixedData = fwCast(8, a);
+        Value * scalableDataBase = ConstantAggregateZero::get(scalable16xi8Ty);
+        Value * scalableData = CreateCall(insertData->getFunctionType(), insertData,
+                                           {scalableDataBase, fixedData, getInt64(0)});
+
+        Value * scalableIdxBase = ConstantAggregateZero::get(scalable16xi8Ty);
+        Value * scalableIdx = CreateCall(insertData->getFunctionType(), insertData,
+                                          {scalableIdxBase, gatherIdx, getInt64(0)});
+
+        Function * tbl = Intrinsic::getDeclaration(getModule(), Intrinsic::aarch64_sve_tbl,
+                                                     {scalable16xi8Ty});
+        Value * scalableResult = CreateCall(tbl->getFunctionType(), tbl,
+                                             {scalableData, scalableIdx});
+
+        Function * extractResult = Intrinsic::getDeclaration(getModule(), Intrinsic::vector_extract,
+                                                               {fixed16xi8Ty, scalable16xi8Ty});
+        Value * fixedResult = CreateCall(extractResult->getFunctionType(), extractResult,
+                                          {scalableResult, getInt64(0)});
+        Value * gathered = fwCast(8, fixedResult);
+
+        Value * zeroMask = CreateSExt(isSelected, fixed16xi8Ty);
+        return simd_and(gathered, zeroMask);
+    }
+    return IDISA_ARM_Builder::mvmd_expand(fw, a, select_mask);
 }
 
 }
